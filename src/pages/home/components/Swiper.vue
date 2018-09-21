5f5ebfdb4f9df98576c5e689fc5ebdb04bcaee8e
dzg <646337191@qq.com>
2018-09-21 12:05:20 +0800

    change

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,41 @@
+<template>
+<div class="wrapper">
+<swiper :options="swiperOption" class="wrapper-swiper">
+    <swiper-slide v-for="item in imgList" :key='item.id'>
+        <img class="wrapper-swiper" :src="item.imgUrl" alt="">
+    </swiper-slide>
+    <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+</div>
+</template>
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      imgList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/b8/16b0e9c77a76e102.jpg_750x200_4863de12.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/9b/8f79dbd1e0941302.jpg_750x200_55829659.jpg'
+      }]
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active
+  background: #fff
+.wrapper
+  width: 100%
+  height: 0
+  overflow: hidden
+  padding-bottom: 37.5%
+  .wrapper-swiper
+    width: 100%
+</style>
