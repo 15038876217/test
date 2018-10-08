d4e4e4c817382a66ed2ece455e28befeec33e0e6
dzg <646337191@qq.com>
2018-10-08 15:32:15 +0800

    icons

--- src/main.js
+++ src/main.js
@@ -13,7 +13,6 @@ import 'styles/iconfont.css'
 // 轮播
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'swiper/dist/css/swiper.css'
-
 Vue.config.productionTip = false
 fastClick.attach(document.body)
 Vue.use(VueAwesomeSwiper)
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,17 +2,26 @@
 <div>
    <home-header></home-header>
    <home-swiper></home-swiper>
+   <home-icons :list="iconList"></home-icons>
 </div>
 </template>
 <script>
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
+import HomeIcons from './components/Icons'
 export default {
   name: 'Home',
   components: {
     HomeHeader,
-    HomeSwiper
+    HomeSwiper,
+    HomeIcons
+  },
+  data () {
+    return {
+      iconList: []
+    }
   }
 }
 </script>
-<style></style>
+<style>
+</style>
