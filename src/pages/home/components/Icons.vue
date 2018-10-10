28d28e87a967c1044c0319078c4245f3cce66a64
dzg <646337191@qq.com>
2018-10-10 14:39:53 +0800

    recommend

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -1,60 +1,60 @@
 <template>
 <div class="icons">
-  <swiper> 
+  <swiper>
     <swiper-slide v-for="(page,index) of pages" :key="index">
-      <div class='icons-item' v-for="item in page" :key="item.id"> 
+      <div class='icons-item' v-for="item in page" :key="item.id">
         <div class="icons-img">
            <img class='icons-img-content' :src="item.imgUrl" alt="">
         </div>
         <p class='icons-desc'>{{item.desc}}</p>
       </div>
-    </swiper-slide>  
-  </swiper>   
+    </swiper-slide>
+  </swiper> 
 </div>
 </template>
 <script>
 export default {
   name: 'HomeIcons',
   data () {
-      return {
-          iconList: [{
-            id: '0001',
-            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
-            desc: '景点门票'
-          }, {
-            id: '0002',
-            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
-            desc: '一日游'
-          }, {
-            id: '0003',
-            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
-            desc: '上海必游'
-          }, {
-            id: '0004',
-            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/8246f27355943202.png',
-            desc: '游乐场' 
-          }, {
-            id: '0005',
-            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/ea/01d081dacb03cc02.png',
-            desc: '赏秋色' 
-          }, {
-            id: '0006',
-            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1805/57/1e29afd06f881102.png',
-            desc: '普陀山'  
-          }, {
-            id: '0007',
-            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/b1/528a9e80403b8c02.png',
-            desc: '上海欢乐谷'  
-          }, {
-            id: '0008',
-            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/47/c2b659e048b11602.png',
-            desc: '动物园'  
-          }, {
-            id: '0009',
-            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/47/c2b659e048b11602.png',
-            desc: '动物园'  
-          }]
-      }
+    return {
+      iconList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '景点门票'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+        desc: '一日游'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
+        desc: '上海必游'
+      }, {
+        id: '0004',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/8246f27355943202.png',
+        desc: '游乐场' 
+      }, {
+        id: '0005',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/ea/01d081dacb03cc02.png',
+        desc: '赏秋色' 
+      }, {
+        id: '0006',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1805/57/1e29afd06f881102.png',
+        desc: '普陀山'  
+      }, {
+        id: '0007',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/b1/528a9e80403b8c02.png',
+        desc: '上海欢乐谷'  
+      }, {
+        id: '0008',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/47/c2b659e048b11602.png',
+        desc: '动物园'  
+      }, {
+        id: '0009',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/47/c2b659e048b11602.png',
+        desc: '动物园'  
+      }]
+    }
   },
   computed: {
     pages () {
