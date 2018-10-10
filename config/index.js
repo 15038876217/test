26a2f2a07d31081811eaf0fd6d58eb3ca0c330c9
dzg <646337191@qq.com>
2018-10-10 17:36:44 +0800

    index finish

--- config/index.js
+++ config/index.js
@@ -3,14 +3,21 @@
 // see http://vuejs-templates.github.io/webpack for documentation.
 
 const path = require('path')
-
+// webpack-dev-server
 module.exports = {
   dev: {
 
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/api': {
+        target: 'http://localhost:8080',
+        pathRewrite:{
+          '^/api': '/static/mock'
+        }
+      }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="wrapper">
-<swiper :options="swiperOption">
-    <swiper-slide v-for="item in imgList" :key='item.id'>
+<swiper :options="swiperOption" v-if="showSwiper">
+    <swiper-slide v-for="item in list" :key='item.id'>
         <img class="swiper-img" :src="item.imgUrl" alt="">
     </swiper-slide>
     <div class="swiper-pagination"  slot="pagination"></div>
@@ -11,19 +11,21 @@
 <script>
 export default {
   name: 'HomeSwiper',
+  props: {
+    list: Array
+  },
   data () {
     return {
       swiperOption: {
         pagination: '.swiper-pagination',
         loop: true
-      },
-      imgList: [{
-        id: '0001',
-        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/b8/16b0e9c77a76e102.jpg_750x200_4863de12.jpg'
-      }, {
-        id: '0002',
-        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/9b/8f79dbd1e0941302.jpg_750x200_55829659.jpg'
-      }]
+      }
+    }
+  },
+  computed: {
+    // first problem slove
+    showSwiper () {
+      return this.list.length
     }
   }
 }
