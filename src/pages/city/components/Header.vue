513c407fee23304d91b2ecf37b697dc353b2a7ea
dzg <646337191@qq.com>
2018-10-11 10:56:03 +0800

    cityinit

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="header">
+      城市选择
+      <router-link to="/">
+      <div class="iconfont header-back">&#xe624;</div>
+      </router-link>
+  </div>
+</template>
+<script>
+export default {
+    name: 'CityHeader'
+}
+</script>
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+.header
+  position: relative
+  height: .86rem
+  line-height: .86rem
+  text-align: center
+  color: #fff
+  background: $bgColor
+  .header-back
+    width: .64rem
+    font-size: .4rem
+    text-align: center
+    position: absolute 
+    left: 0
+    top: 0
+    color: #fff
+</style>
